85bda0d3028d7dfcacd5a8cd051938d8b79388ff
yamakeeeeeeeeen <yamakeeeeeeeeen@gmail.com>
2020-12-26 02:35:24 +0900

    feat: 最低限の検索機能の実装

--- renderer/pages/index.tsx
+++ renderer/pages/index.tsx
@@ -1,7 +1,41 @@
-import { FC } from 'react';
+import { FC, useEffect } from 'react';
+import { PageContent } from '~/components/PageContent';
+import { FormProvider, useForm, useFieldArray } from 'react-hook-form';
+import { ArrayElement } from '~/types/ArrayElement';
+
+const initialUrl = 'https://google.com/';
+
+export type Inputs = {
+  Pages: { Url: string }[];
+};
+export type Page = ArrayElement<Inputs['Pages']>;
 
 const IndexPage: FC = () => {
-  return <p>Home</p>;
+  const methods = useForm<Inputs>({
+    defaultValues: {
+      Pages: [{ Url: initialUrl }, { Url: initialUrl }],
+    },
+  });
+  const { fields } = useFieldArray<Page>({
+    control: methods.control,
+    name: 'Pages',
+  });
+
+  const data = methods.getValues();
+  useEffect(() => {
+    console.log('data', data);
+    console.log('fields', fields);
+  }, [data]);
+
+  return (
+    <div style={{ height: '100vh' }}>
+      <FormProvider {...methods}>
+        {fields.map((page, index) => (
+          <PageContent key={page.id} pageData={page} index={index} />
+        ))}
+      </FormProvider>
+    </div>
+  );
 };
 
 export default IndexPage;
